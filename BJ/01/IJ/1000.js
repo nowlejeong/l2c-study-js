f4b1d08c7729ae37998e38a7d299b856fdb00656
iljinjung <72493043+iljinjung@users.noreply.github.com>
2022-04-15 23:08:15 +0900

    soleved 1000.js

--- BJ/01/IJ/1000.js
+++ BJ/01/IJ/1000.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+// import { readFileSync } from "fs";
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//첫째 줄에 A+B를 출력한다.
+
+// const input = readFileSync("/dev/stdin").toString().split(" ").map(Number);
+const input = [1, 2];
+let answer = input.reduce((a, b) => a + b);
+
+console.log(answer);
